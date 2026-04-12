79aedc0e194c0faa74444cd09f5b68ac12dad6b7
Markus Luik <ml9179@nyu.edu>
2026-04-11 20:30:53 -0400

    Commented RoundManager.cs

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class RoundManager : MonoBehaviour
 {
+    //Canvas prefab that signals round end
     [SerializeField] private GameObject goalCanvas;
     private GameObject currentGoalCanvas;
     
+    /// <summary>
+    /// Triggers when player ENTERS Goal.
+    /// Reported by Goal.
+    /// Spawns goalCanvas and gets its reference.
+    /// </summary>
+    /// <param name="entityName">Name of entity (usually player but set in Goal.cs) that entered Goal</param>
     public void ReportGoalEnter(string entityName)
     {
         Debug.Log($"{entityName} entered goal.");
         currentGoalCanvas = Instantiate(goalCanvas);
     }
     
+    /// <summary>
+    /// Triggers when player EXITS Goal.
+    /// Reported by Goal
+    /// Destroys previously spawned currentGoalCanvas
+    /// </summary>
+    /// <param name="entityName">Name of entity (usually player but set in Goal.cs) that exited Goal</param>
     public void ReportGoalExit(string entityName)
     {
         Debug.Log($"{entityName} exited goal.");
