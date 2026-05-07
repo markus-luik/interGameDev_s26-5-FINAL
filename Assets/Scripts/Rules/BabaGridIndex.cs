809e737bc60c649e7ba7fde5169225f79eca850e
Markus Luik <ml9179@nyu.edu>
2026-05-07 00:17:40 -0400

    OperatorBehavior.cs works and can detect Neighbour's entityName!

--- Assets/Scripts/Rules/BabaGridIndex.cs
+++ Assets/Scripts/Rules/BabaGridIndex.cs
@@ -1,67 +1,67 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-public class BabaGridIndex : MonoBehaviour {
-    public static BabaGridIndex Instance;
-
-    private Dictionary<Vector2Int, List<GridEntity>> _cells = new();
-    private List<GridEntity> _allEntities = new();
-
-    private NewGridManager2D _grid;
-
-    private void Awake() {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
-        _grid = FindObjectOfType<NewGridManager2D>();
-    }
-
-    public void Register(GridEntity entity) {
-        if (!_allEntities.Contains(entity))
-            _allEntities.Add(entity);
-        if (!_cells.ContainsKey(entity.gridPos))
-            _cells[entity.gridPos] = new List<GridEntity>();
-        _cells[entity.gridPos].Add(entity);
-    }
-
-    public void Unregister(GridEntity entity) {
-        _allEntities.Remove(entity);
-        if (_cells.ContainsKey(entity.gridPos))
-            _cells[entity.gridPos].Remove(entity);
-    }
-
-    public List<GridEntity> GetAt(Vector2Int pos) {
-        if (_cells.TryGetValue(pos, out var list))
-            return list;
-        return new List<GridEntity>();
-    }
-
-    public IEnumerable<GridEntity> GetAll() {
-        return _allEntities;
-    }
-
-    public void GridChanged() {
-        RebuildIndex();
-    }
-
-    private void RebuildIndex() {
-        _cells.Clear();
-        foreach (var entity in _allEntities) {
-            // Sync gridPos from NewBlock2D's world position via the grid snapper
-            var block = entity.GetComponent<NewBlock2D>();
-            if (block != null && _grid != null) {
-                Vector2 snapped = _grid.SnapToGrid(block.TargetPos);
-                entity.gridPos = new Vector2Int(
-                    Mathf.RoundToInt(snapped.x),
-                    Mathf.RoundToInt(snapped.y)
-                );
-            }
-            Register(entity);
-        }
-    }
-
-    [ContextMenu("(Debug) Print all Entity locations")]
-    public void DebugPrintAll() {
-        foreach (var entity in GetAll())
-            Debug.Log($"{entity.type.entityName} at {entity.gridPos}");
-    }
-}
+// using UnityEngine;
+// using System.Collections.Generic;
+//
+// public class BabaGridIndex : MonoBehaviour {
+//     public static BabaGridIndex Instance;
+//
+//     private Dictionary<Vector2Int, List<GridEntity>> _cells = new();
+//     private List<GridEntity> _allEntities = new();
+//
+//     private NewGridManager2D _grid;
+//
+//     private void Awake() {
+//         if (Instance == null) Instance = this;
+//         else Destroy(gameObject);
+//         _grid = FindObjectOfType<NewGridManager2D>();
+//     }
+//
+//     public void Register(GridEntity entity) {
+//         if (!_allEntities.Contains(entity))
+//             _allEntities.Add(entity);
+//         if (!_cells.ContainsKey(entity.gridPos))
+//             _cells[entity.gridPos] = new List<GridEntity>();
+//         _cells[entity.gridPos].Add(entity);
+//     }
+//
+//     public void Unregister(GridEntity entity) {
+//         _allEntities.Remove(entity);
+//         if (_cells.ContainsKey(entity.gridPos))
+//             _cells[entity.gridPos].Remove(entity);
+//     }
+//
+//     public List<GridEntity> GetAt(Vector2Int pos) {
+//         if (_cells.TryGetValue(pos, out var list))
+//             return list;
+//         return new List<GridEntity>();
+//     }
+//
+//     public IEnumerable<GridEntity> GetAll() {
+//         return _allEntities;
+//     }
+//
+//     public void GridChanged() {
+//         RebuildIndex();
+//     }
+//
+//     private void RebuildIndex() {
+//         _cells.Clear();
+//         foreach (var entity in _allEntities) {
+//             // Sync gridPos from NewBlock2D's world position via the grid snapper
+//             var block = entity.GetComponent<NewBlock2D>();
+//             if (block != null && _grid != null) {
+//                 Vector2 snapped = _grid.SnapToGrid(block.TargetPos);
+//                 entity.gridPos = new Vector2Int(
+//                     Mathf.RoundToInt(snapped.x),
+//                     Mathf.RoundToInt(snapped.y)
+//                 );
+//             }
+//             Register(entity);
+//         }
+//     }
+//
+//     [ContextMenu("(Debug) Print all Entity locations")]
+//     public void DebugPrintAll() {
+//         foreach (var entity in GetAll())
+//             Debug.Log($"{entity.type.entityName} at {entity.gridPos}");
+//     }
+// }
--- Assets/Scripts/Rules/GridEntity.cs
+++ Assets/Scripts/Rules/GridEntity.cs
@@ -4,34 +4,19 @@ public class GridEntity : MonoBehaviour {
     [Header("Entity definition")]
     public EntityType type;
 
-    [Header("Grid position")]
-    public Vector2Int gridPos;
-
     // Runtime properties — reset and repopulated every turn by RuleParser
-    [HideInInspector] public bool isYou;
-    [HideInInspector] public bool isWin;
-    [HideInInspector] public bool isPush;
-    [HideInInspector] public bool isStop;
-    [HideInInspector] public bool isSink;
-    [HideInInspector] public bool isDefeat;
+    public bool isYou;
+    public bool isWin;
+    public bool isPush;
+    public bool isStop;
+    public bool isSink;
+    public bool isDefeat;
 
     private void Awake() {
         if (type != null && type.isTextBlock)
             isPush = true;
     }
 
-    private void Start() {
-        // Derive gridPos from NewBlock2D's snapped world position
-        var block = GetComponent<NewBlock2D>();
-        if (block != null) {
-            var grid = FindObjectOfType<NewGridManager2D>();
-            if (grid != null) {
-                Vector2 snapped = grid.SnapToGrid(block.TargetPos);
-                gridPos = new Vector2Int(Mathf.RoundToInt(snapped.x), Mathf.RoundToInt(snapped.y));
-            }
-        }
-        BabaGridIndex.Instance.Register(this);
-    }
 
     public void ClearProperties() {
         isYou = isWin = isStop = isSink = isDefeat = false;
