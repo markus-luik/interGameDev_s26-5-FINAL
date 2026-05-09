db07a31638a7d9193c3b86b7bec5ad420d4b3935
Markus Luik <ml9179@nyu.edu>
2026-05-08 21:28:52 -0400

    Flag works, scene changing works, goal canvas edited

--- Assets/Scripts/Rules/RoundManager.cs
+++ Assets/Scripts/Rules/RoundManager.cs
@@ -3,9 +3,77 @@ using UnityEngine.SceneManagement;
 
 public class RoundManager : MonoBehaviour
 {
+    public static RoundManager Instance;
+    //Scene variables
+    private int currentSceneIndex = 0;
+    private int nextSceneIndex = 0;
+    //Finishing logic
+    private bool playerFinished = false;
     //Canvas prefab that signals round end
     [SerializeField] private GameObject goalCanvas;
     private GameObject currentGoalCanvas;
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)//If RoundManager exists && it is not itself, destroy that other game manager
+        {
+            Debug.Log("Other RoundManager detected, will destroy...");
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            
+        }
+    }
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //SCENE INDEXES
+        Debug.Log($"OnSceneLoaded: {scene.name}");
+        Debug.Log(mode);
+        Debug.Log("Setting scene indexes:");
+        //Current scene index
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log($"Current scene: {currentSceneIndex}");
+        //Next scene index
+        nextSceneIndex = currentSceneIndex + 1;
+        Debug.Log($"Next scene: {nextSceneIndex}");
+        Debug.Log("FINISHED setting Scene Indexes");
+
+        ReportGoalExit("Player");
+    }
+    
+    void LoadNextScene()
+    {
+        if(playerFinished && currentSceneIndex > 0 || currentSceneIndex == 0 || Input.GetKey(KeyCode.LeftShift)){ //If player finished in one of the levels OR player is on start screen OR holding Left Shift overrride
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+                {
+                    Debug.Log("Loading next scene...");
+                    SceneManager.LoadScene(nextSceneIndex);
+                }
+                else
+                {
+                    nextSceneIndex = 0;
+                    Debug.Log("LAST scene! Loading 0th scene...");
+                    SceneManager.LoadScene(nextSceneIndex);
+                }
+
+                ReportGoalExit("Player");
+            }
+        }
+    }
     
     //--------------------------------- GOAL ENTER & EXIT
     /// <summary>
@@ -18,6 +86,7 @@ public class RoundManager : MonoBehaviour
     {
         Debug.Log($"{entityName} entered goal.");
         currentGoalCanvas = Instantiate(goalCanvas);
+        playerFinished = true;
     }
     
     /// <summary>
@@ -30,6 +99,7 @@ public class RoundManager : MonoBehaviour
     {
         Debug.Log($"{entityName} exited goal.");
         Destroy(currentGoalCanvas);
+        playerFinished = false;
     }
     //---------------------------------
     
@@ -53,7 +123,9 @@ public class RoundManager : MonoBehaviour
     //--------------------------------- UPDATE
     void Update()
     {
-        ReloadScene();
+        //ReloadScene();
+        LoadNextScene();
     }
     //---------------------------------
 }
+
